3aba2ff54a3a6886d63c4bb498d677d5278e372c
abdullah-900 <58865017+abdullah-900@users.noreply.github.com>
2023-11-26 00:27:32 +0200

    fix broken icons

--- src/Achievements.tsx
+++ src/Achievements.tsx
@@ -9,14 +9,14 @@ const Achievements = () => {
       <div className="flex  flex-col gap-10">
         <div className="flex flex-col lg:flex-row items-center gap-[1.875rem]">
         <div className="flex gap-4">
-            <img src="/public/achieve/Icon.svg" alt="" />
+            <img src="/achieve/Icon.svg" alt="" />
           <div className="flex flex-col">
             <h3 className="text-[1.75rem] text-Neutral/D_Grey font-bold leading-9">2,245,341</h3>
             <p className="text-Neutral/Grey leading-6">Members</p>
           </div>
         </div>
         <div className="flex gap-4">
-            <img src="/public/achieve/Icon-1.svg" alt="" />
+            <img src="/achieve/Icon-1.svg" alt="" />
           <div className="flex flex-col">
           <h3 className="text-[1.75rem] text-Neutral/D_Grey font-bold leading-9">46,328</h3>
             <p className="text-Neutral/Grey leading-6">Clubs</p>
@@ -25,14 +25,14 @@ const Achievements = () => {
         </div>
         <div className="flex flex-col lg:flex-row items-center gap-[1.875rem]">
         <div className="flex gap-4">
-            <img src="/public/achieve/Icon-2.svg" alt="" />
+            <img src="/achieve/Icon-2.svg" alt="" />
           <div className="flex flex-col">
           <h3 className="text-[1.75rem] text-Neutral/D_Grey font-bold leading-9">828,867</h3>
             <p className="text-Neutral/Grey leading-6">Event Bookings</p>
           </div>
         </div>
         <div className="flex gap-4">
-            <img src="/public/achieve/Icon-3.svg" alt="" />
+            <img src="/achieve/Icon-3.svg" alt="" />
           <div className="flex flex-col">
           <h3 className="text-[1.75rem] text-Neutral/D_Grey font-bold leading-9">1,926,436</h3>
             <p className="text-Neutral/Grey leading-6">Payments</p>
