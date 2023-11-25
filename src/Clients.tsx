c1c76836724099a6c7c7b3ae45a0374a154b05b6
abdullah-900 <58865017+abdullah-900@users.noreply.github.com>
2023-11-26 00:12:47 +0200

    add images to public folder

--- src/Clients.tsx
+++ src/Clients.tsx
@@ -4,9 +4,9 @@ const Clients = () => {
       <h2 className="text-[2.25rem] font-semibold leading-[2.75rem] text-Neutral/D_Grey">Our Clients</h2>
       <p className="leading-6 text-Neutral/Grey mt-2">We have been working with some Fortune 500+ clients</p>
       <div className=" w-full justify-between items-center self-stretch flex mt-4 h-[6.125rem]  ">
-        <img src="./clients section/Logo-4.svg" alt="" />
-        <img src="./clients section/Logo-5.svg" alt="" />
-        <img src="./clients section/Logo-6.svg" alt="" />
+        <img src="./Logo-4.svg" alt="" />
+        <img src="./Logo-5.svg" alt="" />
+        <img src="./Logo-6.svg" alt="" />
         <img src="./clients section/Logo.svg" alt="" />
         <img src="./clients section/Logo-1.svg" alt="" />
         <img src="./clients section/Logo-2.svg" alt="" />
