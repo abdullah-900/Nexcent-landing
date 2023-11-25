48a064fa6cf88bcaafcefb4ab4b6299fa8af3709
abdullah-900 <58865017+abdullah-900@users.noreply.github.com>
2023-11-26 00:07:41 +0200

    fix broken images on production

--- src/Clients.tsx
+++ src/Clients.tsx
@@ -4,13 +4,13 @@ const Clients = () => {
       <h2 className="text-[2.25rem] font-semibold leading-[2.75rem] text-Neutral/D_Grey">Our Clients</h2>
       <p className="leading-6 text-Neutral/Grey mt-2">We have been working with some Fortune 500+ clients</p>
       <div className=" w-full justify-between items-center self-stretch flex mt-4 h-[6.125rem]  ">
-        <img src="/public/clients section/Logo-4.svg" alt="" />
-        <img src="/public/clients section/Logo-5.svg" alt="" />
-        <img src="/public/clients section/Logo-6.svg" alt="" />
-        <img src="/public/clients section/Logo.svg" alt="" />
-        <img src="/public/clients section/Logo-1.svg" alt="" />
-        <img src="/public/clients section/Logo-2.svg" alt="" />
-        <img src="/public/clients section/Logo-3.svg" alt="" />
+        <img src="/clients section/Logo-4.svg" alt="" />
+        <img src="/clients section/Logo-5.svg" alt="" />
+        <img src="/clients section/Logo-6.svg" alt="" />
+        <img src="/clients section/Logo.svg" alt="" />
+        <img src="/clients section/Logo-1.svg" alt="" />
+        <img src="/clients section/Logo-2.svg" alt="" />
+        <img src="/clients section/Logo-3.svg" alt="" />
       </div>
     </section>
   );
